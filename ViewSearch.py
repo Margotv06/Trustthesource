1c9deb79f23ba1d0d031062297c38a9bd4f01299
Michaël van der Veen <pjvanderveen@msn.com>
2015-11-17 12:44:34 +0100

    Main, ViewSearch, ViewWindow created

---
+++ ViewSearch.py
@@ -0,0 +1,21 @@
+"""
+  _____               _     _____ _            ____
+ |_   _| __ _   _ ___| |_  |_   _| |__   ___  / ___|  ___  _   _ _ __ ___ ___
+   | || '__| | | / __| __|   | | | '_ \ / _ \ \___ \ / _ \| | | | '__/ __/ _ \
+   | || |  | |_| \__ \ |_    | | | | | |  __/  ___) | (_) | |_| | | | (_|  __/
+   |_||_|   \__,_|___/\__|   |_| |_| |_|\___| |____/ \___/ \__,_|_|  \___\___|
+
+Created by Daniel Slobben, Michael van der Veen & Margot Verleg
+
+Creation Date: 
+    
+Class: 
+    
+Contributers:
+
+Description:
+    This is the Main class that would be run to start the program.
+
+ChangeLog:
+
+"""
---
+++ ViewWindow.py
@@ -0,0 +1,17 @@
+"""
+  _____               _     _____ _            ____
+ |_   _| __ _   _ ___| |_  |_   _| |__   ___  / ___|  ___  _   _ _ __ ___ ___
+   | || '__| | | / __| __|   | | | '_ \ / _ \ \___ \ / _ \| | | | '__/ __/ _ \
+   | || |  | |_| \__ \ |_    | | | | | |  __/  ___) | (_) | |_| | | | (_|  __/
+   |_||_|   \__,_|___/\__|   |_| |_| |_|\___| |____/ \___/ \__,_|_|  \___\___|
+
+Created by Daniel Slobben, Michael van der Veen & Margot Verleg
+
+Creation Date: 17-11-2015
+Class: Main
+Description:
+    This is the Main class that would be run to start the program.
+
+ChangeLog:
+17-11-2015_12:34: Creation of this Class.
+"""
